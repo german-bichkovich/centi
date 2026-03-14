a86f31806d235e82f11a1663c2b0761bad1b28bc
German Bichkovich <g.bichkovich@gmail.com>
2026-03-14 13:33:59 +0200

    simplify special/function helpers in builtins

--- src/builtins.lisp
+++ src/builtins.lisp
@@ -23,110 +23,114 @@
 ;; (evaluate form environment)
 ;; Evaluate form in provided or current environment.
 (define "evaluate"
-  (wrap (special (form . rest) env
-          (eval form (if (consp rest)
-                         (car rest)
-                         env)))))
+  (wrap (special args env
+          (destructuring-bind (form &optional (env env)) args
+            (eval form env)))))
 
 ;; (if 'test 'then 'else)
 ;; If test is true, evaluate then form, otherwise evaluate else form.
 (define "if"
-  (special (test then else) env
-    (if (cbool->bool (eval test env))
-        (eval then env)
-        (eval else env))))
+  (special args env
+    (destructuring-bind (test then else) args
+      (if (cbool->bool (eval test env))
+          (eval then env)
+          (eval else env)))))
 
 ;; (symbol name)
 ;; Create a symbol that is not interned.
 (define "symbol"
-  (function (name)
-    (symbol name)))
+  (function args
+    (symbol (car args))))
 
 ;; (symbol? object)
 ;; Check if object is a symbol.
 (define "symbol?"
-  (function (object)
-    (bool->cbool (or (eq object nil)
-                     (symbol? object)))))
+  (function args
+    (let ((object (car args)))
+      (bool->cbool (or (symbol? object)
+                       (eq object nil))))))
 
 ;; (symbol:name symbol)
 ;; Return a string representing a symbol name.
 (define "symbol:name"
-  (function (symbol)
-    (symbol-name symbol)))
+  (function args
+    (symbol-name (car args))))
 
 ;; (symbol:intern name)
 ;; Intern a symbol with provided name.
 ;; Two different outputs of (intern "x") are always the same objects.
 (define "symbol:intern"
-  (function (name)
-    (intern name)))
+  (function args
+    (intern (car args))))
 
 ;; (pair? object)
 ;; Check if object is a pair.
 (define "pair?"
-  (function (object)
-    (bool->cbool (consp object))))
+  (function args
+    (bool->cbool (consp (car args)))))
 
 ;; (pair head tail)
 ;; Construct a pair.
 (define "pair"
-  (function (head tail)
-    (cons head tail)))
+  (function args
+    (cons (car args) (cadr args))))
 
 ;; (pair:head! pair value)
 ;; Store value in head slot of a pair.
 (define "pair:head!"
-  (function (pair object)
-    (rplaca pair object)
-    pair))
+  (function args
+    (destructuring-bind (pair object) args
+      (rplaca pair object)
+      pair)))
 
 ;; (pair:tail! pair value)
 ;; Store value in tail slot of a pair.
 (define "pair:tail!"
-  (function (pair object)
-    (rplacd pair object)
-    pair))
+  (function args
+    (destructuring-bind (pair object) args
+      (rplacd pair object)
+      pair)))
 
 ;; (function? object)
 ;; Check if object is a function.
 (define "function?"
-  (function (object)
-    (bool->cbool (function? object))))
+  (function args
+    (bool->cbool (function? (car args)))))
 
 ;; (special? object)
 ;; Check if object is a special form.
 (define "special?"
-  (function (object)
-    (bool->cbool (special? object))))
+  (function args
+    (bool->cbool (special? (car args)))))
 
 ;; (special 'ptree 'ebind . 'body)
 ;; Create a new special form.
 (define "special"
-  (special (ptree ebind . body) env
-    (special-new :ebind ebind
-                 :ptree ptree
-                 :body (special-remove-declaration body)
-                 :environment env
-                 :metadata (special-extract-metadata body))))
+  (special args env
+    (destructuring-bind (ptree ebind . body) args
+      (special-new :ebind ebind
+                   :ptree ptree
+                   :body (special-remove-declaration body)
+                   :environment env
+                   :metadata (special-extract-metadata body)))))
 
 ;; (unwrap function)
 ;; Return a special form with same body as one function has.
 (define "unwrap"
-  (function (object)
-    (unwrap object)))
+  (function args
+    (unwrap (car args))))
 
 ;; (wrap special)
 ;; Return a function with same body as provided special has.
 (define "wrap"
-  (function (object)
-    (wrap object)))
+  (function args
+    (wrap (car args))))
 
 ;; (number? object)
 ;; Check if object is a number.
 (define "number?"
-  (function (object)
-    (bool->cbool (numberp object))))
+  (function args
+    (bool->cbool (numberp (car args)))))
 
 ;; (- number)
 ;; (- 1) => -1
@@ -159,109 +163,116 @@
 ;; (<< number n)
 ;; Bit shift number by n bits to the left.
 (define "<<"
-  (function (number n)
-    (ash number n)))
+  (function args
+    (ash (car args) (cadr args))))
 
 ;; (>> number n)
 ;; Bit shift number by n bits to the right.
 (define ">>"
-  (function (number n)
-    (ash number (- n))))
+  (function args
+    (ash (car args) (- (cadr args)))))
 
 ;; (array? object)
 ;; Check if object is an array.
 (define "array?"
-  (function (object)
-    (bool->cbool (and (arrayp object)
-                      (eq t (array-element-type object))))))
+  (function args
+    (let ((object (car args)))
+      (bool->cbool (and (arrayp object)
+                        (eq t (array-element-type object)))))))
 
 ;; (array:length array)
 ;; Get length of an array.
 (define "array:length"
-  (function (a)
-    (length a)))
+  (function args
+    (length (car args))))
 
 ;; (array::new length default)
 ;; Primitive constructor for a general array.
 (define "array::new"
-  (function (length &optional (default nil default?))
-    (make-array length
-                :initial-element (if default?
-                                     default
-                                     (intern "nil"))
-                :element-type t)))
+  (function args
+    (destructuring-bind (length &optional (default nil default?)) args
+      (make-array length
+                  :initial-element (if default?
+                                       default
+                                       (intern "nil"))
+                  :element-type t))))
 
 ;; (array:get array index)
 ;; (array:get array index default)
 ;; Get element of an array at index.
 (define "array:get"
-  (function (a n . rest)
-    (if (< -1 n (length a))
-        (aref a n)
-        (if (consp rest)
-            (car rest)
-            (intern "nil")))))
+  (function args
+    (destructuring-bind (a n . rest) args
+      (if (< -1 n (length a))
+          (aref a n)
+          (or (car rest) (intern "nil"))))))
 
 ;; (array:set! array index value)
 ;; Set element of an array at index.
 (define "array:set!"
-  (function (a n v)
-    (setf (aref a n) v)
-    a))
+  (function args
+    (destructuring-bind (a n v) args
+      (setf (aref a n) v)
+      a)))
 
 ;; (string? object)
 ;; Check if object is a string.
 (define "string?"
-  (function (object)
-    (bool->cbool (and (arrayp object)
-                      (eq 'character (array-element-type object))))))
+  (function args
+    (let ((object (car args)))
+      (bool->cbool (and (arrayp object)
+                        (eq 'character
+                            (array-element-type object)))))))
 
 ;; (string:length string)
 ;; Get length of a string.
 (define "string:length"
-  (function (string)
-    (length string)))
+  (function args
+    (length (car args))))
 
 ;; (string::new length default)
 ;; A primitive constructor for a string.
 (define "string::new"
-  (function (length &optional (default 0))
-    (make-array length
-                :initial-element (code-char default)
-                :element-type 'character)))
+  (function args
+    (destructuring-bind (length &optional (default 0)) args
+      (make-array length
+                  :initial-element (code-char default)
+                  :element-type 'character))))
 
 ;; (string:get array index)
 ;; Get element of an array at index.
 ;; TODO get nth character vs get nth byte?
 (define "string:get"
-  (function (a n)
-    (char-code (aref a n))))
+  (function args
+    (destructuring-bind (a n) args
+      (char-code (aref a n)))))
 
 ;; (string:set! array index value)
 ;; Set element of an array at index.
 (define "string:set!"
-  (function (a n v)
-    (setf (aref a n) (code-char v))
-    a))
+  (function args
+    (destructuring-bind (a n v) args
+      (setf (aref a n) (code-char v))
+      a)))
 
 ;; (string:code string)
 ;; Return the code encoded in first character of the string.
 (define "string:code"
-  (function (s)
-    (char-code (aref s 0))))
+  (function args
+    (char-code (aref (car args) 0))))
 
 ;; (string:character code)
 ;; Return a string of length 1 with first
 ;; element being provided code.
 (define "string:character"
-  (function (c)
-    (format nil "~a" (code-char c))))
+  (function args
+    (format nil "~a" (code-char (car args)))))
 
 ;; (record? object)
 ;; Check if object is a record.
 (define "record?"
-  (function (o)
-    (bool->cbool (record? o))))
+  (function args
+    (bool->cbool (record? (car args)))))
 
 ;; (record type . slots)
 ;; Create a record with particular type, filling all of the slots.
@@ -272,33 +283,35 @@
 ;; (record:type record)
 ;; Get type of a record
 (define "record:type"
-  (function (r)
-    (record-type r)))
+  (function args
+    (record-type (car args))))
 
 ;; (record:length record)
 ;; Get length of a record.
 (define "record:length"
-  (function (r)
-    (record-length r)))
+  (function args
+    (record-length (car args))))
 
 ;; (record:get record index)
 ;; Get nth field of a record.
 ;; 0th field stores the type.
 (define "record:get"
-  (function (r n)
-    (record-get r n)))
+  (function args
+    (destructuring-bind (r n) args
+      (record-get r n))))
 
 ;; (record:set! record index value)
 ;; Set nth field of a record to value.
 (define "record:set!"
-  (function (r n v)
-    (record-set! r n v)))
+  (function args
+    (destructuring-bind (r n v) args
+      (record-set! r n v))))
 
 ;; (environment? object)
 ;; Create a new environment with specified parent environment.
 (define "environment?"
-  (function (object)
-    (environment? object)))
+  (function args
+    (bool->cbool (environment? (car args)))))
 
 ;; (environment)
 ;; (environment parent)
@@ -312,81 +325,88 @@
 ;; (environment:set! environment symbol value)
 ;; Set value of a symbol in environment.
 (define "environment:set!"
-  (function (env symbol value)
-    (environment-set! env symbol value)))
+  (function args
+    (destructuring-bind (env symbol value) args
+      (environment-set! env symbol value))))
 
 ;; (environment:find environment symbol)
 ;; Find an environment in which symbol is bound.
 (define "environment:find"
-  (function (env symbol)
-    (let ((e (environment-find env symbol)))
-      (or e (intern "false")))))
+  (function args
+    (destructuring-bind (env symbol) args
+      (let ((e (environment-find env symbol)))
+        (or e (intern "false"))))))
 
 ;; (write-byte byte)
 ;; Write a byte to standard output.
 (define "write-byte"
-  (function (byte)
-    (format t "~a" (code-char byte))
+  (function args
+    (format t "~a" (code-char (car args)))
     (intern "nil")))
 
 ;; (write string) => nil
 ;; Write string to standard output.
 (define "write"
-  (function (string)
-    (loop for c across string do (princ c))
+  (function args
+    (loop for c across (car args) do (princ c))
     (intern "nil")))
 
 ;; (:print object)
 ;; A primitive printer. Extended with generics in-langauge.
 (define ":print"
-  (function (object)
-    (print object)))
+  (function args
+    (print (car args))))
 
 ;; (== . objects)
 ;; Check of each of the objects are same object.
 (define "=="
-  (function objects
-    (bool->cbool (let ((first (car objects)))
-                   (dolist (o objects t)
+  (function args
+    (bool->cbool (let ((first (car args)))
+                   (dolist (o args t)
                      (or (eq first o) (return)))))))
 
 ;; (exit code)
 ;; Kill the interpreter and make it return the provided exit code.
 (define "exit"
-  (function (code)
-    (uiop:quit code)))
+  (function args
+    (uiop:quit (car args))))
 
 ;; TODO remove
 ;; (die message)
 ;; Print message and kill current process.
 (define "die"
-  (function (message)
-    (error "~a" message)))
+  (function args
+    (error "~a" (car args))))
 
 (define "special::ebind"
-  (function (special)
-    (assert (special? special))
-    (special-ebind special)))
+  (function args
+    (let ((special (car args)))
+      (assert (special? special))
+      (special-ebind special))))
 
 (define "special::body"
-  (function (special)
-    (assert (special? special))
-    (special-body special)))
+  (function args
+    (let ((special (car args)))
+      (assert (special? special))
+      (special-body special))))
 
 (define "special::ptree"
-  (function (special)
-    (assert (special? special))
-    (special-ptree special)))
+  (function args
+    (let ((special (car args)))
+      (assert (special? special))
+      (special-ptree special))))
 
 (define "special::environment"
-  (function (special)
-    (assert (special? special))
-    (special-environment special)))
+  (function args
+    (let ((special (car args)))
+      (assert (special? special))
+      (special-environment special))))
 
 (define "special::metadata"
-  (function (special)
-    (assert (special? special))
-    (special-metadata special)))
+  (function args
+    (let ((special (car args)))
+      (assert (special? special))
+      (special-metadata special))))
 
 ;;; Globals ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
--- src/datatypes/callable.lisp
+++ src/datatypes/callable.lisp
@@ -60,3 +60,17 @@ that forces evaluation of its arguments."
                       (t
                        (cons k v))))
       ()))
+
+;;; Convinience constructors ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro special (ptree ebind &rest body)
+  `(special-new
+    :ebind ,(if (consp ebind) ebind `',ebind)
+    :ptree 'args
+    :environment 'nil
+    :body (lambda (args env)
+            (declare (ignorable args env))
+            ,@body)))
+
+(defmacro function (parameter &rest body)
+  `(wrap (special ,parameter (centi:intern "nil") . ,body)))
